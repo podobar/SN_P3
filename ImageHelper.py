6fec75488bc47641d2e7893b3de74ede568f90de
podobar <robert.podoba73@gmail.com>
2020-11-19 16:53:21 +0100

    Added: ImageHelper.py:     + to display black and white images which are visualization of array-data; FileHelper.py:     + to read and transform data from .csv file;     + to retrieve size of an image from file name training datasets (.csv files)

---
+++ ImageHelper.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+class ImageHelper:
+    @staticmethod
+    def draw_image(data):
+        plt.imshow(data, cmap='gray', vmin=-1, vmax=1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.show()
--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from FileHelper import FileHelper as fh
+from ImageHelper import ImageHelper as ih
+
+test_set_file_paths = [
+    'Data/animals-14x9.csv',
+    'Data/large-25x25.csv',
+    'Data/large-25x25.plus.csv',
+    'Data/large-25x50.csv',
+    'Data/letters-14x20.csv',
+    'Data/letters-abc-8x12.csv',
+    'Data/OCRA-12x30-cut.csv',
+    'Data/small-7x7.csv'
+]
+
 if __name__ == '__main__':
-    print("init")
+    train_file_path = test_set_file_paths[] #Insert index to show data set as black&white images
+    img_size = fh.get_size_from_name(train_file_path)
+    raw_data = fh.load(train_file_path)
+    for arr in raw_data:
+        reshaped_data = fh.reshape(data=arr, shape=img_size)
+        ih.draw_image(reshaped_data)
+    print('End')
