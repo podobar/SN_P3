a31daa05fdd11ac3b3a3ff0e4d1ff56d4d04b0f1
podobar <robert.podoba73@gmail.com>
2020-11-21 00:06:06 +0100

    Modified: 1. Scaling of clickable buttons - now they are much smaller 2. After using Train -> Choose training set buttons are recreated basing on size present in file name. Missing: 1. Training of network and implementation of network itself

--- main.py
+++ main.py
@@ -1,54 +1,64 @@
 from FileHelper import FileHelper as fh
 from ImageHelper import ImageHelper as ih
 import tkinter as tk
+from tkinter import filedialog
 root = tk.Tk()
-test_set_file_paths = [
-    'Data/animals-14x9.csv',
-    'Data/large-25x25.csv',
-    'Data/large-25x25.plus.csv',
-    'Data/large-25x50.csv',
-    'Data/letters-14x20.csv',
-    'Data/letters-abc-8x12.csv',
-    'Data/OCRA-12x30-cut.csv',
-    'Data/small-7x7.csv'
-]
+root.title("Hopfield's network")
+
+#Global variables
+img_size = [0]
+training_data = [0]
+buttons = list()
+#
 
 
-def __load_file():
-    return
+def load_training_set_and_train():
+    train_file_path = filedialog.askopenfilename(initialdir="/Data", title="Choose training set", filetypes=(("csv files", "*.csv"),))
+    if train_file_path:
+        global img_size
+        global training_data
+
+        img_size = fh.get_size_from_name(train_file_path)
+        raw_data = fh.load(train_file_path)
+        #IF you want to see what you're training the network on, then toggle breakpoint on for below and use debug mode. Uncomment the code first of course.
+        # for arr in raw_data:
+        #     img = fh.reshape(data=arr, shape=img_size)
+        #     ih.draw_image(img)
+        training_data = raw_data
+        #TODO: Train here
+        #
+        clear_buttons()
 
 
-def __switch_background(index: int):
-    __buttons[index]['bg'] = 'white' if __buttons[index]['bg'] == 'black' else 'black'
-    __buttons[index].update()
+def clear_buttons():
+    global buttons
+    global img_size
+    for button in buttons:
+        button.destroy()
+    buttons.clear()
+    if len(img_size) == 2:
+        for i in range(img_size[0]):
+            for j in range(img_size[1]):
+                b = tk.Button(root,
+                           text="",
+                           width=2,
+                           height=1,
+                           bg='black',
+                           command=lambda _index=i * img_size[1] + j: switch_background(index=_index)
+                           )
+                b.grid(row=i, column=j)
+                buttons.append(b)
+
+
+def switch_background(index: int):
+    buttons[index]['bg'] = 'white' if buttons[index]['bg'] == 'black' else 'black'
+    buttons[index].update()
+
 
 if __name__ == '__main__':
-    # train_file_path = test_set_file_paths[] #Insert index to show data set as black&white images
-    # img_size = fh.get_size_from_name(train_file_path)
-    # raw_data = fh.load(train_file_path)
-    # for arr in raw_data:
-    #     reshaped_data = fh.reshape(data=arr, shape=img_size)
-    #     ih.draw_image(reshaped_data)
-    # print('End')
-    N = 5
-    M = 10
-    root.title("Hopfield's network")
-    __buttons = list()
     __menu = tk.Menu(root)
     root.config(menu=__menu)
     __train_menu = tk.Menu(__menu)
     __menu.add_cascade(label="Train", menu=__train_menu)
-    __train_menu.add_command(label='Choose training file and train', command=__load_file)
-
-    for i in range(N):
-        for j in range(M):
-            b = tk.Button(root,
-                       width=10,
-                       height=10,
-                       bg='black' if i % 2 == 0 else 'white',
-                       command=lambda _index=i * M + j: __switch_background(index=_index)
-                       )
-            b.grid(row=i, column=j)
-            __buttons.append(b)
-
+    __train_menu.add_command(label='Choose training file and train', command=load_training_set_and_train)
     root.mainloop()
