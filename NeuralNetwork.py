cea9ea002b835c10460e206837a0864d389affd1
podobar <robert.podoba73@gmail.com>
2020-11-27 20:47:11 +0100

    Added: 1.NeuralNetwork.py  +Hopfield's network model with seed(for repeatable randomization in async method)  +Hebb training method  +Oja training method (in progress)  +sync and async test methods (in progress) Modified 1. main.py  ~ visualization commented for network testing purposes

---
+++ NeuralNetwork.py
@@ -0,0 +1,44 @@
+#https://github.com/yogonza524/HopfieldNetwork/tree/master/src
+# https://www.yumpu.com/xx/document/read/43026957/141-metody-uczenia-sieci
+import numpy as np
+class HopfieldNetwork:
+
+    def __init__(self, n: int, seed: int):
+        self.n = n #size of vector
+        self.seed = seed
+        self.w = np.zeros((n,n)) #weights, initially 0.0 <-> 0.0
+        self.patterns = list()
+
+        return
+
+    def train_hebb(self, training_set):
+        self.patterns = training_set
+        for x in training_set:
+            self.w = np.add(self.w, np.subtract(np.outer(x,x), np.identity(len(x))))
+        self.w = self.w * 1/self.n
+
+    # def train_oja(self, training_set, learning_rate: float):
+    #     self.patterns = training_set
+    #     for x in training_set:
+    #                 V = np.sum(w[i,j]*x[j]) #i-loop
+    #
+    #         self.w[i,j] += learning_rate * V * (x[i] - V*self.w[i,j])
+
+    def test_sync(self):
+        return
+    def test_async(self):
+        return
+    # def update_sync_hebb(self, x):
+    #     #Update in upper-right part first, then rewrite to lower-left
+    #     for i in range(1, self.n):
+    #         for j in range (i):
+    #             self.w[i,j]
+    #     return
+    # def update_async(self, x):
+    #     indices = np.arange(0, self.n, step=1)
+    #     random.Random(self.seed).shuffle(indices)
+    #     z = np.outer(x, x)
+    #     z = np.subtract(z, np.identity(len(x)))
+    #     for i in indices:
+
+        return
--- main.py
+++ main.py
@@ -1,64 +1,69 @@
 from FileHelper import FileHelper as fh
 from ImageHelper import ImageHelper as ih
+from NeuralNetwork import HopfieldNetwork
 import tkinter as tk
 from tkinter import filedialog
-root = tk.Tk()
-root.title("Hopfield's network")
-
-#Global variables
-img_size = [0]
-training_data = [0]
-buttons = list()
+# root = tk.Tk()
+# root.title("Hopfield's network")
 #
-
-
-def load_training_set_and_train():
-    train_file_path = filedialog.askopenfilename(initialdir="/Data", title="Choose training set", filetypes=(("csv files", "*.csv"),))
-    if train_file_path:
-        global img_size
-        global training_data
-
-        img_size = fh.get_size_from_name(train_file_path)
-        raw_data = fh.load(train_file_path)
-        #IF you want to see what you're training the network on, then toggle breakpoint on for below and use debug mode. Uncomment the code first of course.
-        # for arr in raw_data:
-        #     img = fh.reshape(data=arr, shape=img_size)
-        #     ih.draw_image(img)
-        training_data = raw_data
-        #TODO: Train here
-        #
-        clear_buttons()
-
-
-def clear_buttons():
-    global buttons
-    global img_size
-    for button in buttons:
-        button.destroy()
-    buttons.clear()
-    if len(img_size) == 2:
-        for i in range(img_size[0]):
-            for j in range(img_size[1]):
-                b = tk.Button(root,
-                           text="",
-                           width=2,
-                           height=1,
-                           bg='black',
-                           command=lambda _index=i * img_size[1] + j: switch_background(index=_index)
-                           )
-                b.grid(row=i, column=j)
-                buttons.append(b)
-
-
-def switch_background(index: int):
-    buttons[index]['bg'] = 'white' if buttons[index]['bg'] == 'black' else 'black'
-    buttons[index].update()
+# #Global variables
+# img_size = [0]
+# training_data = [0]
+# buttons = list()
+# #
+#
+#
+# def load_training_set_and_train():
+#     train_file_path = filedialog.askopenfilename(initialdir="/Data", title="Choose training set", filetypes=(("csv files", "*.csv"),))
+#     if train_file_path:
+#         global img_size
+#         global training_data
+#
+#         img_size = fh.get_size_from_name(train_file_path)
+#         raw_data = fh.load(train_file_path)
+#         #IF you want to see what you're training the network on, then toggle breakpoint on for below and use debug mode. Uncomment the code first of course.
+#         # for arr in raw_data:
+#         #     img = fh.reshape(data=arr, shape=img_size)
+#         #     ih.draw_image(img)
+#         training_data = raw_data
+#         #TODO: Train here
+#         #
+#         clear_buttons()
+#
+#
+# def clear_buttons():
+#     global buttons
+#     global img_size
+#     for button in buttons:
+#         button.destroy()
+#     buttons.clear()
+#     if len(img_size) == 2:
+#         for i in range(img_size[0]):
+#             for j in range(img_size[1]):
+#                 b = tk.Button(root,
+#                            text="",
+#                            width=2,
+#                            height=1,
+#                            bg='black',
+#                            command=lambda _index=i * img_size[1] + j: switch_background(index=_index)
+#                            )
+#                 b.grid(row=i, column=j)
+#                 buttons.append(b)
+#
+#
+# def switch_background(index: int):
+#     buttons[index]['bg'] = 'white' if buttons[index]['bg'] == 'black' else 'black'
+#     buttons[index].update()
 
 
 if __name__ == '__main__':
-    __menu = tk.Menu(root)
-    root.config(menu=__menu)
-    __train_menu = tk.Menu(__menu)
-    __menu.add_cascade(label="Train", menu=__train_menu)
-    __train_menu.add_command(label='Choose training file and train', command=load_training_set_and_train)
-    root.mainloop()
+    # __menu = tk.Menu(root)
+    # root.config(menu=__menu)
+    # __train_menu = tk.Menu(__menu)
+    # __menu.add_cascade(label="Train", menu=__train_menu)
+    # __train_menu.add_command(label='Choose training file and train', command=load_training_set_and_train)
+    # root.mainloop()
+    network = HopfieldNetwork(n=9, seed=1010, threshold=0.1)
+    network.update_async([1,2,3])
+    network.update_sync([1,2,3])
+    print('done')
