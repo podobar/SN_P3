1b8fcb3d4e664f44245a5b010541b77de0bb8985
podobar <robert.podoba73@gmail.com>
2020-12-18 02:55:51 +0100

    Added: 1. NeuralNetwork.py  +Method for testing training set
    Modified:
    1.NeuralNetwork.py
     ~Oja learning method reimplemented (using Hebb's method for start weights, also using signum function for overflow problem)
     ~Test methods separated and set private
    2.Main.py
     ~Divided menus for train/test mode
    3.FileHelper.py, ImageHelper.py - moved classes to Utils.py
    4.Utils.py
     ~File naming rule (with time and example identifier) introduced

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,19 +1,35 @@
 #https://github.com/yogonza524/HopfieldNetwork/tree/master/src
 # https://www.yumpu.com/xx/document/read/43026957/141-metody-uczenia-sieci
 import numpy as np
-from UtilFunctions import UtilFunctions
-from ImageHelper import ImageHelper
-from FileHelper import FileHelper
+import glob
+import os
+
+from Utils import Functions
+from Utils import ImageHelper
+from Utils import FileHelper
+
 class HopfieldNetwork:
-
-    def __init__(self, n: int, seed: int, shape):
+    def __init__(self, n: int, shape, seed: int = 100):
         self.n = n #size of vector
-        self.seed = seed
-        self.w = np.zeros((n,n)) #weights, initially 0.0 <-> 0.0
-        self.patterns = list()
         self.shape = shape
+        self.seed = seed
 
+        self.w = np.zeros((n, n))  # weights, initially 0.0 <-> 0.0
+        self.patterns = list()
         return
+
+    def output(self):
+        ret = np.zeros(self.n)
+        for i in range(self.n):
+            ret[i] = 1 if np.sum(self.w[i,:]) >= 0 else -1
+        return ret
+
+    def energy(self, x: list):
+        ret_energy = 0.0
+        for i in range(self.n):
+            for j in range(self.n):
+                ret_energy += -0.5 * self.w[i,j] * x[i] * x[j]
+        return ret_energy
 
     def train_hebb(self, training_set):
         self.patterns = training_set
@@ -21,56 +37,75 @@ class HopfieldNetwork:
             self.w = np.add(self.w, np.subtract(np.outer(x,x), np.identity(len(x))))
         self.w = self.w * 1/self.n
 
-    def train_oja(self, training_set, learning_rate: float):
-        self.patterns = training_set
-        for x in training_set:
-            for i in range(self.n):
-                for j in range(self.n):
-                    if i==j:
-                        continue
-                    V = np.sum(self.w[:,j])*x[j] #i-loop
-                    self.w[i, j] += learning_rate * V * (x[i] - V * self.w[i, j])
+    def train_oji(self, training_set, learning_rate: float = 0.8, epochs: int = 1):
+        self.train_hebb(training_set)
+        for epoch in range(epochs):
+            for x in training_set:
+                for i in range(self.n):
+                    V = np.sign(np.sum(self.w[i,:]*x[:]))
+                    for j in range(self.n):
+                        if i == j:
+                            continue
+                        self.w[i, j] += learning_rate * V * (x[i] - V * self.w[i, j])
 
-    def test_sync(self, x):
-        max_iterations = np.power(4, np.power(2, len(self.patterns)-1))
-        i = 0
-        is_loop = False
-        found = False
-        steps_made = list()
-        steps_made.append(x)
-        ImageHelper.save_image(data=FileHelper.reshape(data=x, shape=self.shape), index=i)
-        while is_loop == False and found == False and i < max_iterations:
-            x = UtilFunctions.step(np.multiply(self.w, x))
-            if self.patterns.__contains__(x):
-                found = True
-            elif x in steps_made:
-                is_loop = True
-            steps_made.append(x)
+    def test_training_set(self, is_sync: bool=True):
+        for pattern in self.patterns:
+            self.test(pattern, is_sync, is_training_pattern=True)
+
+
+    def test(self, x:list, is_sync: bool = True, is_training_pattern: bool = False):
+        return self.__test_sync(x, is_training_pattern) if is_sync is True else self.__test_async(x, is_training_pattern)
+
+
+    def __test_sync(self, x: list, is_training_pattern: bool = False):
+        pattern_id = self.patterns.index(x) if is_training_pattern else None
+        i=0
+        energy_log = list()
+        energy_log.append(self.energy(x))
+        steps_log = list()
+        steps_log.append(x)
+        ImageHelper.save_image(data=FileHelper.reshape(data=x, shape=self.shape), index=i, pattern_id=pattern_id)
+        while True:
+            i+=1
+            x = Functions.step(list(np.dot(self.w, x)))
+            energy_log.append(self.energy(x))
+            steps_log.append(x)
+            ImageHelper.save_image(data=FileHelper.reshape(data=x, shape=self.shape), index=i, pattern_id=pattern_id)
+            if energy_log[i] >= energy_log[i-1]:
+    #Network reached stable state or is looped
+                if x in self.patterns:
+                    found_index = self.patterns.index(x)
+                    test_result = f"Rozpoznano ({found_index}). "
+                else:
+                    test_result = "Nie rozpoznano. "
+                break
+        ImageHelper.save_energy_plot(data=energy_log, result=test_result, pattern_id=pattern_id)
+        return x
+
+    def __test_async(self, x: list, is_training_pattern: bool = False):
+        pattern_id = self.patterns.index(x) if is_training_pattern else None
+        i=0
+        energy_log = list()
+        energy_log.append(self.energy(x))
+        steps_log = list()
+        steps_log.append(x)
+        ImageHelper.save_image(data=FileHelper.reshape(data=x, shape=self.shape), index=i, pattern_id=pattern_id)
+        while True:
+            np.random.seed(self.seed + i)
             i += 1
-            ImageHelper.save_image(data = FileHelper.reshape(data=x, shape=self.shape), index= i)
-        if is_loop or i == max_iterations:
-            return 1
-        elif found:
-            return 0
-
-    def test_async(self, x:list): #TODO
-        indices = np.arange(0, self.n, step=1)
-        np.random.Random(self.seed).shuffle(indices)
-        steps_made = list()
-        while x not in steps_made:
-            for i in indices:
-                return #TODO
-        return
-    # def update_sync_hebb(self, x):
-    #     #Update in upper-right part first, then rewrite to lower-left
-    #     for i in range(1, self.n):
-    #         for j in range (i):
-    #             self.w[i,j]
-    #     return
-    # def update_async(self, x):
-    #     indices = np.arange(0, self.n, step=1)
-    #     random.Random(self.seed).shuffle(indices)
-    #     z = np.outer(x, x)
-    #     z = np.subtract(z, np.identity(len(x)))
-    #     for i in indices:
-
+            for j in range(self.n):
+                rand_index = np.random.randint(0, self.n)
+                x[rand_index] = Functions.step(list(np.dot(self.w, x)))[rand_index]
+            energy_log.append(self.energy(x))
+            steps_log.append(x)
+            ImageHelper.save_image(data=FileHelper.reshape(data=x, shape=self.shape), index=i, pattern_id=pattern_id)
+            if energy_log[i] >= energy_log[i - 1]:
+                # Network reached stable state or is looped
+                if x in self.patterns:
+                    found_index = self.patterns.index(x)
+                    test_result = f"Rozpoznano ({found_index}). "
+                else:
+                    test_result = "Nie rozpoznano. "
+                break
+        ImageHelper.save_energy_plot(data=energy_log, result=test_result, pattern_id=pattern_id)
+        return x
--- UtilFunctions.py
+++
@@ -1,6 +0,0 @@
-class UtilFunctions:
-    @staticmethod
-    def step(x: list):
-        for i in range(len(x)):
-            x[i] = 1.0 if x[i] >= 0 else -1.0
-        return x
